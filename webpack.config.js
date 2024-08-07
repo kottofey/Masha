a322e02c182b10f771b788645955911654f109c7
Kottofey (BigBrother) <romeokontakt@gmail.com>
2024-08-08 00:21:36 +0300

    Настройки проекта

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,44 @@
 const path = require('path');
 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
 	mode: 'development',
 	entry: {
-		index: ['@babel/polyfill', './scripts/index.js'],
+		index: ['@babel/polyfill', './js/index.js'],
 	},
 	output: {
-		filename: '[name].[contenthash].js',
+		filename: './scripts/[name].[contenthash].js',
 		path: path.resolve(__dirname, 'dist'),
 		clean: true,
 	},
+	devtool: 'source-map',
 	plugins: [
 		new htmlWebpackPlugin({
 			template: './index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'styles/masha.css',
 		}),
 	],
 	module: {
 		rules: [
 			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
+				test: /\.(?:js|mjs|cjs)$/,
+				include: path.resolve(__dirname, 'src/js'),
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [['@babel/preset-env', { targets: 'defaults' }]],
+					},
+				},
+			},
+			{
+				test: /\.(scss|sass|css)$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
 			},
 			{
 				test: /\.(html?)$/i,
@@ -31,6 +47,16 @@ module.exports = {
 			{
 				test: /\.(jpe?g|png|gif|svg)$/i,
 				type: 'asset/resource',
+				generator: {
+					filename: 'images/[name]-[contenthash][ext]',
+				},
+			},
+			{
+				test: /\.(woff|ttf|eot)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'fonts/[name]-[contenthash][ext]',
+				},
 			},
 		],
 	},
